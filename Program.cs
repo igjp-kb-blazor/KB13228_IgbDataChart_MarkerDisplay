b0b9a2492b41b506afdf122f32e515c3693163f3
TomoyaWashio <148823993+TomoyaWashio@users.noreply.github.com>
2024-04-09 11:49:37 +0900

    初回コミット

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using IgniteUI.Blazor.Controls;
+using KB13228_IgbDataChart_MarkerDisplay;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddIgniteUIBlazor(
+    typeof(IgbDataLegendModule),
+    typeof(IgbScatterAreaSeriesModule),
+    typeof(IgbCategoryChartModule)
+    );
+
+await builder.Build().RunAsync();
